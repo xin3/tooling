f76075ea1064f136a3471e47b0b4dc5094a72d7a
Alex Wang <ruleroland@gmail.com>
2021-08-03 16:36:29 -0700

    fix: update comment

--- targeted-js-file.js
+++ targeted-js-file.js
@@ -1,51 +1,50 @@
-// this is the final js file
+// this is a sample js file to be minified & renamed
 
-const https = require('https');
-const fs = require('fs');
-const Config = require('./config');
+const https = require("https");
+const Config = require("./config");
 
 const creatCommit = () => {
   const reqBody = JSON.stringify({
-    branch: 'master',
-    start_branch: 'master',
-    commit_message: 'ci: modify version & change log',
+    branch: "master",
+    start_branch: "master",
+    commit_message: "ci: modify version & change log",
     actions: [
       {
-        action: 'update',
-        file_path: './package.json',
-        content: 'new content'
+        action: "update",
+        file_path: "./package.json",
+        content: "new content",
       },
       {
-        action: 'update',
-        file_path: './package-lock.json',
-        content: 'new content'
+        action: "update",
+        file_path: "./package-lock.json",
+        content: "new content",
       },
       {
-        action: 'update',
-        file_path: './CHANGELOG.md',
-        content: 'new content'
-      }
-    ]
+        action: "update",
+        file_path: "./CHANGELOG.md",
+        content: "new content",
+      },
+    ],
   });
 
   const request = https.request(
     {
-      method: 'POST',
+      method: "POST",
       hostname: Config.HOST_NAME,
       path: `/api/v4/projects/${Config.PROJECT_ID}/repository/commits`,
-      headers: { Authorization: `Bearer ${Config.PERSONAL_TOKEN}` }
+      headers: { Authorization: `Bearer ${Config.PERSONAL_TOKEN}` },
     },
-    respose => {
+    (respose) => {
       respose
-        .on('data', chunk => {
-          console.log('getting release response...');
+        .on("data", (chunk) => {
+          console.log("getting release response...");
           console.log(chunk.toString());
         })
-        .on('end', () => {});
+        .on("end", () => {});
     }
   );
 
-  request.on('error', err => console.log(err));
+  request.on("error", (err) => console.log(err));
 
   request.write(reqBody);
 
